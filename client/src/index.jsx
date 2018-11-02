e055240a1ace076ae98bca0f29eced4cb5b075d7
sean-bellinger <onething.sean@gmail.com>
2018-11-02 16:05:11 -0700

    Added state and component did mount, along with a get request to query the server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-
     };
+  }
+  componentDidMount() {
+    axios.get('/home', {
+    params: {
+      houseName: "Ducimus aut officiis qui voluptas rerum quaerat."
+    }
+  }).then(res => console.log(res));
+
   }
   render () {
     return (
-      <div> Hey you better work mister!</div>
+      <div> You better work!</div>
       );
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('house-description'));
+
+/*
+plug in params into the CDM
+
+*/
