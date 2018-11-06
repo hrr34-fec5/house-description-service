2756608b740cb025f8ec7efed74b57c9544225aa
sean-bellinger <onething.sean@gmail.com>
2018-11-06 11:19:50 -0800

    Added components and rendered to the page.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import Overview from './Components/overview.jsx'
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
+      currentHouse: {}
     };
   }
   componentDidMount() {
@@ -13,12 +15,17 @@ class App extends React.Component {
     params: {
       houseName: "Ducimus aut officiis qui voluptas rerum quaerat."
     }
-  }).then(res => console.log(res));
+  }).then(res => {
+    let data = res.data;
+    this.setState({currentHouse: data})
+  });
 
   }
   render () {
     return (
-      <div> You better work!</div>
+      <div>
+      <Overview data={this.state.currentHouse} />
+      </div>
       );
   }
 }
@@ -28,4 +35,12 @@ ReactDOM.render(<App />, document.getElementById('house-description'));
 /*
 plug in params into the CDM
 
+on line 25 you will begin to render
+**detailed console.logs
+search react render
+
+client => server => database
+client <= server <= database
+
 */
+
