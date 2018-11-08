258ac7563ef6daf463e1f41a89f2ba0d75199dc2
sean-bellinger <onething.sean@gmail.com>
2018-11-08 12:05:42 -0800

    Changed props flow to force tests to work

--- client/src/Components/overview.jsx
+++ client/src/Components/overview.jsx
@@ -8,16 +8,16 @@ class Overview extends React.Component {
   render(){
     return (
       <div>
-        <h1 className="header">{this.props.data.houseName}</h1>
+        <h1 className="header">{this.props.houseName}</h1>
         <div className="numberOf">
-          <i className="fas fa-users"></i><span className="icons">{this.props.data.numberOfGuests} guests</span>
-          <i className="fas fa-door-open"></i><span className="icons">{this.props.data.numberOfBedrooms} bedrooms</span>
-          <i className="fas fa-bed"></i><span className="icons">{this.props.data.numberOfBeds} beds</span>
-          <i className="fas fa-bath"></i><span className="icons">{this.props.data.numberOfBaths} baths</span>
+          <i className="fas fa-users"></i><span className="icons">{this.props.numberOfGuests} guests</span>
+          <i className="fas fa-door-open"></i><span className="icons">{this.props.numberOfBedrooms} bedrooms</span>
+          <i className="fas fa-bed"></i><span className="icons">{this.props.numberOfBeds} beds</span>
+          <i className="fas fa-bath"></i><span className="icons">{this.props.numberOfBaths} baths</span>
         </div>
         <div className="highlights"> <div className="title">Highlights</div> <br/>
-        {this.props.data.Highlights}</div>
-        <div className="description"><div className="title">The space</div> <br/>{this.props.data.description}</div>
+        {this.props.highlights}</div>
+        <div className="description"><div className="title">The space</div> <br/>{this.props.description}</div>
         <div className="contact">Contact host</div>
         <div className="border-line"></div>
         <div className="amenities"><div className="title">Amenities</div><br/>
