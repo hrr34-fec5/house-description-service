e410acd52bca31eff9fdbeb2832fd3309c86991d
sean-bellinger <onething.sean@gmail.com>
2018-11-08 12:06:09 -0800

    Changed import flow to force tests to work

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,42 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import Overview from './Components/overview.jsx';
-import BelowReviews from './Components/belowReviews.jsx';
-
-class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      currentHouse: {}
-    };
-  }
-  componentDidMount() {
-    axios.get('/home', {
-    params: {
-      houseName: "Ducimus aut officiis qui voluptas rerum quaerat."
-    }
-  }).then(res => {
-    let data = res.data;
-    this.setState({currentHouse: data})
-  });
-
-  }
-  render () {
-    return (
-      <div>
-        <div>
-          <Overview data={this.state.currentHouse} />
-        </div>
-        <div>
-          <BelowReviews data={this.state.currentHouse} />
-        </div>
-      </div>
-      );
-  }
-}
+import App from './Components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('house-description'));
+
+
 
 /*
 plug in params into the CDM
